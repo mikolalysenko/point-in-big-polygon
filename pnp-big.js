b8963bfbbc27f0f4a75dd153406a45c36535c818
Mikola Lysenko <mikolalysenko@gmail.com>
2015-01-29 22:13:07 -0600

    fix bugs in interface

--- pnp-big.js
+++ pnp-big.js
@@ -1,84 +1,151 @@
-"use strict"
-
 module.exports = preprocessPolygon
 
-var orient = require("robust-orientation")
-var makeSlabs = require("slab-decomposition")
+var orient = require('robust-orientation')
+var makeSlabs = require('slab-decomposition')
+var makeIntervalTree = require('interval-tree-1d')
+var bsearch = require('binary-search-bounds')
 
-function dummyFunction(p) {
-  return -1
+function visitInterval() {
+  return true
 }
 
-function createClassifyPoint(segments, slabs, outside, orientation) {
-  function classifyPoint(p) {
-    var index = slabs.castUp(p)
-    if(index < 0) {
-      return outside
+function intervalSearch(table) {
+  return function(x, y) {
+    var tree = table[x]
+    if(tree) {
+      return !!tree.queryPoint(y, visitInterval)
     }
-    var seg = segments[index]
-    if(!orientation) {
-      return orient(p, seg[0], seg[1])
+    return false
+  }
+}
+
+function buildVerticalIndex(segments) {
+  var table = {}
+  for(var i=0; i<segments.length; ++i) {
+    var s = segments[i]
+    var x = s[0][0]
+    var y0 = s[0][1]
+    var y1 = s[1][1]
+    var p = [ Math.min(y0, y1), Math.max(y0, y1) ]
+    if(x in table) {
+      table[x].push(p)
     } else {
-      return orient(p, seg[1], seg[0])
+      table[x] = [ p ]
     }
   }
-  return classifyPoint
+  var intervalTable = {}
+  var keys = Object.keys(table)
+  for(var i=0; i<keys.length; ++i) {
+    var segs = table[keys[i]]
+    intervalTable[keys[i]] = makeIntervalTree(segs)
+  }
+  return intervalSearch(intervalTable)
 }
 
-function preprocessPolygon(loops, orientation) {
-  orientation = !!orientation
+function buildSlabSearch(slabs, coordinates) {
+  return function(p) {
+    var bucket = bsearch.le(coordinates, p[0])
+    if(bucket < 0) {
+      return 1
+    }
+    var root = slabs[bucket]
+    if(!root) {
+      if(bucket > 0 && coordinates[bucket] === p[0]) {
+        root = slabs[bucket-1]
+      } else {
+        return 1
+      }
+    }
+    var lastOrientation = 1
+    while(root) {
+      var s = root.key
+      var o = orient(p, s[0], s[1])
+      if(s[0][0] < s[1][0]) {
+        if(o < 0) {
+          root = root.left
+        } else if(o > 0) {
+          lastOrientation = -1
+          root = root.right
+        } else {
+          return 0
+        }
+      } else {
+        if(o > 0) {
+          root = root.left
+        } else if(o < 0) {
+          lastOrientation = 1
+          root = root.right
+        } else {
+          return 0
+        }
+      }
+    }
+    return lastOrientation
+  }
+}
 
+function classifyEmpty(p) {
+  return 1
+}
+
+function createClassifyVertical(testVertical) {
+  return function classify(p) {
+    if(testVertical(p[0], p[1])) {
+      return 0
+    }
+    return 1
+  }
+}
+
+function createClassifyPointDegen(testVertical, testNormal) {
+  return function classify(p) {
+    if(testVertical(p[0], p[1])) {
+      return 0
+    }
+    return testNormal(p)
+  }
+}
+
+function preprocessPolygon(loops) {
   //Compute number of loops
   var numLoops = loops.length
-  var numSegments = 0
-  for(var i=0; i<numLoops; ++i) {
-    numSegments += loops[i].length
-  }
-
-  //Degenerate case: All loops are empty
-  if(numSegments === 0) {
-    return dummyFunction
-  }
 
   //Unpack segments
-  var segments = new Array(numSegments)
+  var segments = []
+  var vsegments = []
   var ptr = 0
   for(var i=0; i<numLoops; ++i) {
     var loop = loops[i]
     var numVertices = loop.length
     for(var s=numVertices-1,t=0; t<numVertices; s=(t++)) {
-      segments[ptr++] = [loop[s], loop[t]]
+      var a = loop[s]
+      var b = loop[t]
+      if(a[0] === b[0]) {
+        vsegments.push([a,b])
+      } else {
+        segments.push([a,b])
+      }
+    }
+  }
+
+  //Degenerate case: All loops are empty
+  if(segments.length === 0) {
+    if(vsegments.length === 0) {
+      return classifyEmpty
+    } else {
+      return createClassifyVertical(buildVerticalIndex(vsegments))
     }
   }
 
   //Build slab decomposition
   var slabs = makeSlabs(segments)
+  var testSlab = buildSlabSearch(slabs.slabs, slabs.coordinates)
 
-  //Find outer orientation
-  var outside
-  var root = slabs.slabs[0]
-  if(root) {
-    while(root.left) {
-      root = root.left
-    }
-    var h = root.key
-    if(h[0][0] < h[1][0]) {
-      outside = -1
-    } else {
-      outside = 1
-    }
+  if(vsegments.length === 0) {
+    return testSlab
   } else {
-    var h = segments[slabs.horizontal[0][0].index]
-    if(h[0][1] < h[1][1]) {
-      outside = 1
-    } else {
-      outside = -1
-    }
+    return createClassifyPointDegen(
+      buildVerticalIndex(vsegments),
+      testSlab)
   }
-  if(orientation) {
-    outside = -outside
-  }
-
-  //Return classification function
-  return createClassifyPoint(segments, slabs, outside, orientation)
 }
